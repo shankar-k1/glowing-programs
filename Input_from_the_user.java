d1560234947982e1d3c8226bb42e0b41774c3e41
SHANKARA NARAYANAN.K <shan68973@gmail.com>
2023-01-14 17:27:42 +0530

    new

---
+++ Input_from_the_user.java
@@ -0,0 +1,14 @@
+
+public static void InputArray(int[] arr,num){
+        int i;
+        Scanner sc=new Scanner(System.in);
+        for(i=0;i<=num;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<=num;i++){
+            return arr[i];
+        }
+    }
+public static void main(String[] args){
+    
+}
